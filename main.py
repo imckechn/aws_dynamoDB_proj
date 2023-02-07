0f7923420247bb5699eddc468d8d7f933fa58ec3
Ian McKechnie <imckechn@uoguelph.ca>
2023-02-07 14:59:52 -0500

    Added more functionality

--- main.py
+++ main.py
@@ -35,21 +35,125 @@ except:
 
 # Check if the tables exist
 existing_tables = client.list_tables()
+tables = []
 
-if "test_table" in existing_tables['TableNames']:
-    print("Table already exists")
-    table = Table("test_table")
+print("Existing Tables:")
+for tableName in existing_tables["TableNames"]:
+    table = Table(tableName)
     table.loadTable(client_res)
+    tables.append(table)
+    print(tableName)
 
-else:
-    print("creating a table")
-    table = Table('test_table')
-    table.create(client_res, "ISO3", "S", "Country Name", "S")
+print()
+while(True):
+    print("Please enter a command")
+    print("Commands: create, delete, print, print head bulkload, exit, add row, delete row")
+    command = input()
 
-#table.delete_self(client)
+    if command[:4] == "exit":
+        print("Goodbye!")
+        quit()
 
-print("Now filling the table")
-#table.bulk_load_csv("shortlist_area.csv")
+    elif command[:len("create")] == "create":
+        name = input("Table name?" )
+        attyA = input("Attribute A? ")
+        attyAType = input("Attribute A data type (Number or String)? ")
+        attyB = input("Attribute B? ")
+        attyBType = input("Attribute B data type (Number or String)? ")
+        table = Table(name)
+        success = table.create(client_res, attyA, attyAType, attyB, attyBType)
+        if success:
+            tables.append(table)
 
-print("Done")
 
+    elif command[:len("bulkload")] == "bulkload":
+        name = input("Table name? ")
+
+        nameFound = False
+        for table in tables:
+            if table.get_name() == tableName:
+                nameFound = True
+                filename = input("CSV Filename? ")
+                table.bulk_load_csv(filename)
+                break
+
+        if not nameFound:
+            print("Failed to find a table with that name")
+
+    elif command[:len("print head")] == "print head":
+        tableName = input("Table name? ")
+        nameFound = False
+        for table in tables:
+            if table.get_name() == tableName:
+                nameFound = True
+                table.print_head()
+                break
+
+        if not nameFound:
+            print("Failed to find a table with that name")
+
+    elif command[:len("print")] == "print":
+        tableName = input("Table name? ")
+        nameFound = False
+        for table in tables:
+            if table.get_name() == tableName:
+                nameFound = True
+                table.print_all_rows()
+                break
+
+        if not nameFound:
+            print("Failed to find a table with that name")
+
+    elif command[:len("add row")] == "add row":
+        tableName = input("Table name? ")
+        nameFound = False
+        for table in tables:
+            if table.get_name() == tableName:
+                nameFound = True
+                columns = table.get_column_headers()
+                data = []
+
+                for column in columns:
+                    dat = input("Enter data for column '" + column + "': ")
+                    data.append(dat)
+
+                table.add_row(data)
+                break
+
+        if not nameFound:
+            print("Failed to find a table with that name")
+
+
+    elif command[:len("delete row")] == "delete row":
+        tableName = input("Table name? ")
+        nameFound = False
+        for table in tables:
+            if table.get_name() == tableName:
+                nameFound = True
+                columns = table.get_column_headers()
+                data = []
+
+                for column in columns:
+                    dat = input("Enter data for column '" + column + "': ")
+                    data.append(dat)
+
+                table.delete_row(data)
+                break
+
+        if not nameFound:
+            print("Failed to find a table with that name")
+
+
+    elif command[:len("delete")] == "delete":
+        name = input("Table name? ")
+        nameFound = False
+        for table in tables:
+            if table.get_name() == name:
+                table = Table(name)
+                ans = table.delete_self(client)
+                if ans:
+                    tables.remove(name)
+                    print("Table removed")
+
+        if not nameFound:
+            print("Failed to find a table with that name")
--- tables.py
+++ tables.py
@@ -58,8 +58,13 @@ class Table:
     def loadTable(self, dynamo):
         try:
             self.table = dynamo.Table(self.name)
-            attrs = self.table.attribute_definitions
-            print("atters: ", attrs)
+
+            attributes = self.table.attribute_definitions
+            self.column_headers = []
+
+            for atty in attributes:
+                self.column_headers.append(atty['AttributeName'])
+
         except Exception as e:
             print("Error: ", e)
 
@@ -69,8 +74,10 @@ class Table:
     def delete_self(self, client):
         try:
             client.delete_table(TableName=self.name)
+            return True
         except Exception as e:
             print("Error: ", e)
+            return False
 
 
     # This function bulk loads a csv file into a table
@@ -79,20 +86,89 @@ class Table:
     def bulk_load_csv(self, csv_file_name):
         records = json.loads(pd.read_csv(csv_file_name).to_json(orient='records'))
 
-        self.header = records[0].keys()
+        self.column_headers = records[0].keys()
 
         for record in records:
             items = {}
-            for key in self.header:
+            for key in self.column_headers:
                 items[key] = record[key]
 
             self.table.put_item(
                 Item = items
             )
 
-    def add_item(self, item):
-        item.split(",")
+
+    # Adds an item to the table
+    # It takes in the following parameters:
+    # item: the item to add which is all the rows in the table, seperated by commas
+    def add_row(self, item):
+
+        counter = 0
+        table_row = {}
+        for key in self.column_headers:
+            table_row[key] = item[0]
+            counter += 1
 
         self.table.put_item(
-            Item = item
+            Item = table_row
         )
+
+
+    # Deletes an item fro the table
+    # It takes in the following parameters:
+    # item: the item to delete which is all the rows in the table, seperated by commas
+    def delet_row(self, item):
+
+        counter = 0
+        table_row = {}
+        for key in self.column_headers:
+            table_row[key] = item[0]
+            counter += 1
+
+        self.table.delete_item(
+            Key = table_row
+        )
+
+
+    def get_name(self):
+        return self.name
+
+
+    def get_column_headers(self):
+        return self.column_headers
+
+
+    def print_all_rows(self):
+        rows =  self.table.scan()['Items']
+
+        header = rows[0].keys()
+        for elem in header:
+            print(elem, end=", ")
+
+        print()
+        for row in rows:
+            for key in header:
+                print(row[key], end=", ")
+            print()
+
+
+    def print_head(self):
+        print("Printing head")
+        rows =  self.table.scan()['Items']
+
+        header = rows[0].keys()
+        self.column_headers = []
+        for elem in header:
+            self.column_headers.append(elem)
+            print(elem, end=", ")
+
+        print()
+        i = 0
+        for row in rows:
+            if i == 5:
+                return
+
+            for key in header:
+                print(row[key], end=", ")
+            print()
+            i += 1
